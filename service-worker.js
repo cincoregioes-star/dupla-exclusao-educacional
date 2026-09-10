7a9fde4a6ae74a5b079453302887f919e25c9515
cincoregioes-star <cincoregioes@gmail.com>
2026-09-10 11:34:36 -0300

    fix(v24.1): renova cache após correção da tela de bullying

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CORE = "dupla-exclusao-core-v24-1";
-const FULL = "dupla-exclusao-full-v24-1";
+const CORE = "dupla-exclusao-core-v24-2";
+const FULL = "dupla-exclusao-full-v24-2";
 
 const core = [
   "./",
